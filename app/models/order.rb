656e0365e0b7ac7ee1edfebd5ba64f5c1a3e275c
Brian Ho <pilotbear19@gmail.com>
2015-09-18 13:16:37 -0300

    This is the one that has a calendar :P

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,13 +1,19 @@
 class Order < ActiveRecord::Base
   belongs_to :customer
-
+  delegate :full_name, to: :customer,prefix: true
 
   def volumecalc
     if length.nil? || width.nil? || height.nil?
       return 0
     else
-      return ((length*width*height)/1728)
+      return ((length*width*height)/1728).round(2)
     end
+  end
+
+def down_payment_percentage
+
+  return ((down_payment/tml_price)*100)
+
   end
 
 
