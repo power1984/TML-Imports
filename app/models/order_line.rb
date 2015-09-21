18865cdf9603535222c4837ed18b8838d2a8f696
Brian Ho <pilotbear19@gmail.com>
2015-09-21 17:03:38 -0300

    Added OrderStatus

--- app/models/order_line.rb
+++ app/models/order_line.rb
@@ -1,4 +1,34 @@
 class OrderLine < ActiveRecord::Base
   belongs_to :order
   has_one :order_status
+
+
+
+  def volumecalc
+    if length.nil? || width.nil? || height.nil?
+      return 0
+    else
+      return ((length*width*height)/1728).round(2)
+    end
+  end
+
+  def down_payment_percentage
+
+    return ((down_payment/tml_price)*100)
+
+  end
+
+
+
+
+  def freightchoice
+    if freight_type
+      return "Air"
+    else
+      return "Sea"
+    end
+  end
+
+
+
 end
