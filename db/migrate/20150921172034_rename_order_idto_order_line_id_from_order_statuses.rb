0ae4441dbb88cf215d77bfa78630e6f471e82473
Brian Ho <pilotbear19@gmail.com>
2015-09-30 14:38:01 -0300

    Blah

--- db/migrate/20150921172034_rename_order_idto_order_line_id_from_order_statuses.rb
+++
@@ -1,7 +0,0 @@
-class RenameOrderIdtoOrderLineIdFromOrderStatuses < ActiveRecord::Migration
-  def change
-    OrderLine.delete_all
-    Order.delete_all
-    rename_column :order_statuses, :order_id , :order_line_id
-  end
-end
--- db/migrate/20150922001025_rename_freight_type_to_is_air_freight.rb
+++
@@ -1,5 +0,0 @@
-class RenameFreightTypeToIsAirFreight < ActiveRecord::Migration
-  def change
-    rename_column :order_lines, :freight_type, :is_air_freight
-  end
-end
