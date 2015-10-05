57eb685a898e79cacf9ce2449640319c4785435d
Brian Ho <pilotbear19@gmail.com>
2015-10-05 11:55:13 -0300

    10/05/2015 --------------------------------------------------- Devise: - Model - User was created to authenticate and sign up users. - A column was added to Customers named user_id Relationships added Customers: belongs_to :user
    User:
    has_many :customers
    
     Added /config/initializers/devise.rb
     In order to make sign out work
    
    - config.sign_out_via = :delete
    + config.sign_out_via = :get
    
    ---------------------------------------------------
    10/04/2015
    ---------------------------------------------------
    Added gem 'devise' to gemfile
    added "put" to order_lines#ordered so it could update from the checkboxes.
    Changed in my order_lines/index.html from order_date to purchase_date
    Added validations upto order_line
    
    ---------------------------------------------------
    10/03/2015
    ---------------------------------------------------
    Downloaded Bootstrap-datepicker.js for auto-orientation
    
    ---------------------------------------------------
    
    10/02/2015
    ---------------------------------------------------
    Removed is_forwarder from customers[index,form,list,controller]
    
    Permanently removed it from migration file aswell
    
    ---------------------------------------------------
    
    Working Migrations
    
    Fixed Orders Index Page
    Fixed Order_ME Index Page
    Added Rails 12 Factor // hashed it out

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,6 @@
 class CustomersController < ApplicationController
   before_action :set_customer, only: [:show, :edit, :update, :destroy]
-
+ before_action :authenticate_user! ,except: [:index,:show]
 
   # It returns the articles whose titles contain one or more words that form the query
 
